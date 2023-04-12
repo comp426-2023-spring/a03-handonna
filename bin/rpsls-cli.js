70eec77a72fef90730d1bc29a6ed1531dca0ff47
Donna Han <89470080+handonna@users.noreply.github.com>
2023-04-12 15:34:28 -0400

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,49 +1,56 @@
 #!/usr/bin/env node
+
 import minimist from 'minimist';
-import { rpsls } from "../lib/rpsls.js"
+import { rpsls } from "../lib/rpsls.js";
 
-var args = minimist(process.argv.slice(2));
+var argv = minimist(process.argv.slice(2))
 
-const help_message = `Usage: node-rpsls [SHOT]
-Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
-  -h, --help        display this help message and exit
-  -r, --rules       display the rules and exit
-
-Examples:
-  node-rpsls        Return JSON with single player RPSLS result.
-                    e.g. {"player":"rock"}
-  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}
-`;
-
-const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
-- Scissors CUTS Paper
-- Paper COVERS Rock
-- Rock SMOOSHES Lizard
-- Lizard POISONS Spock
-- Spock SMASHES Scissors
-- Scissors DECAPITATES Lizard
-- Lizard EATS Paper
-- Paper DISPROVES Spock
-- Spock VAPORIZES Rock
-- Rock CRUSHES Scissors
-`;
-
-if (args.h || args.rules) {
-        console.log(help_message);
-        process.exit(0);
+function printHelp() {
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`)
 }
 
-if (args.r || args.rules) {
-        console.log(rules);
-        process.exit(0);
+function printRules() {
+    console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`)
 }
 
-let shot = args._[0]
+
+
+if (argv.h|| argv.help) {
+    printHelp()
+    process.exit(0)
+}
+
+if (argv.r|| argv.rules) {
+    printRules()
+    process.exit(0)
+}
 
 try {
-        console.log(JSON.stringify(rpsls(shot)));
+    let result = rpsls((argv._)[0])
+    console.log(JSON.stringify(result))
+    process.exit(0)
 } catch (e) {
-        console.log(rules);
+    printHelp()
+    printRules()
+    process.exit(1)
 }
