9fa103b98f6f433f38b77f4c7f88ab78f097d9d3
Donna Han <89470080+handonna@users.noreply.github.com>
2023-04-12 15:33:19 -0400

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,39 +1,50 @@
 #!/usr/bin/env node
+
 import minimist from 'minimist';
-import { rps } from "../lib/rpsls.js"
+import { rps } from "../lib/rpsls.js";
 
-var args = minimist(process.argv.slice(2));
+var argv = minimist(process.argv.slice(2))
 
-const help_message = `Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPS)
-
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
-
-Examples:
-  node-rps        Return JSON with single player RPS result.
-                  e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}`; 
-
-const rules = `Rules for Rock Paper Scissors:
-
-  - Scissors CUTS Paper
-  - Paper COVERS Rock
-  - Rock CRUSHES Scissors`; 
-
-if (args.h || args.help) {
-	console.log(help_message);
-	process.exit(0);
+function printHelp() {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`)
 }
-if (args.r || args.rules) {
-	console.log(rules);
-	process.exit(0);
+
+function printRules() {
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`)
 }
-let shot = args._[0]
+
+
+
+if (argv.h !== undefined || argv.help !== undefined) {
+    printHelp()
+    process.exit(0)
+}
+
+
+if (argv.r !== undefined || argv.rules !== undefined) {
+    printRules()
+    process.exit(0)
+}
+
 try {
-	console.log(JSON.stringify(rps(shot)));
+    let result = rps((argv._)[0])
+    console.log(JSON.stringify(result))
+    process.exit(0)
 } catch (e) {
-	console.log(help_message);
-	console.log(rules);
+    printHelp()
+    printRules()
+    process.exit(1)
 }
